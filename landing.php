a6e75cb1e5ba785041170ff26be28a647c8569c5
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-07 17:17:19 +0000

    Landing page will be main page for CA

--- landing.php
+++ landing.php
@@ -4,27 +4,29 @@
         <meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
         <title>TBM
         </title>
-
+        <!--all styles and scripts will be contained in these php scripts-->
         <?php require 'utilities/styles.php'; ?>
         <?php require 'utilities/scripts.php'; ?>
     </head>
     <body>
 
-        <?php require 'utilities/header.php'; ?>
+        <div class="row">
+                <?php require 'utilities/header.php'; ?>
+                <?php require 'utilities/toolbar.php'; ?>
+        </div>
         <div class="center-content">
-            <div class="">
+            <div>
                 <h3>Welcome to our lovely internal system.</h3>
-                <p>Take a look at our vast variety of garages and who manage them.
-                </p>
                 
                 
-        <?php require 'utilities/toolbar.php'; ?>
+                
+        
                 
             </div>
 
         </div>
         
-        <?php require 'utilities/footer.php'; ?>
+        <?php require 'utilities/footer.php'; ?> 
 
 
 
