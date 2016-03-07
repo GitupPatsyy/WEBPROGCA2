7f2b7c5682595372898e91763bcace0c484ecd27
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-07 17:16:13 +0000

    View all and view by ID works fine.

--- viewGarage.php
+++ viewGarage.php
@@ -5,7 +5,6 @@
  * Date: 01/12/2015
  * Time: 12:17 PM
  */
-
 require_once 'connection.php';
 require_once 'garageTableGateway.php';
 
@@ -24,48 +23,47 @@ $row = $statement->fetch(PDO::FETCH_ASSOC);
 if (!$row) {
     die("Unable to get Garage By ID");
 }
-
-
 ?>
 
 
 <!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>View Garage by Id</title>
 
-<table class="pure-table pure-table-bordered">
-    <thead>
-    <tr>
-        <th>Address</th>
-        <th>Phone No.</th>
-        <th>Manager Name</th>
-        <th>Garage Name</th>
-        <th>Garage ID</th>
-        <th>Service Date</th>
-        <th>Manager Email</th>
-        <th>Garage URL</th>
-        <th>Over Night?</th>
-    </tr>
-    <?php
+        <!--all styles and scripts will be contained in these php scripts-->
+        <?php require 'utilities/styles.php'; ?>
+        <?php require 'utilities/scripts.php'; ?>
+    </head>
+    <body>
 
-    echo '<tr class="pure-table-odd">';
-    echo '<td>' . $row['garageAddress'] . '</td>';
-    echo '<td>' . $row['phoneNo'] . '</td>';
-    echo '<td>' . $row['managerName'] . '</td>';
-    echo '<td>' . $row['nameofGarage'] . '</td>';
-    echo '<td>' . $row['garageID'] . '</td>';
-    echo '<td>' . $row['dateService'] . '</td>';
-    echo '<td>' . $row['managerEmail'] . '</td>';
-    echo '<td>' . $row['garageURL'] . '</td>';
-    echo '<td>' . $row['overNight'] . '</td>';
-    echo '</tr>';
-
-    ?>
-</table>
-</body>
+        <table class="container">
+            <thead>
+                <tr>
+                    <th>Address</th>
+                    <th>Phone No.</th>
+                    <th>Manager Name</th>
+                    <th>Garage Name</th>
+                    <th>Garage ID</th>
+                    <th>Service Date</th>
+                    <th>Manager Email</th>
+                    <th>Garage URL</th>
+                    <th>Over Night?</th>
+                </tr>
+                <?php
+                echo '<tr class="pure-table-odd">';
+                echo '<td>' . $row['garageAddress'] . '</td>';
+                echo '<td>' . $row['phoneNo'] . '</td>';
+                echo '<td>' . $row['managerName'] . '</td>';
+                echo '<td>' . $row['nameofGarage'] . '</td>';
+                echo '<td>' . $row['garageID'] . '</td>';
+                echo '<td>' . $row['dateService'] . '</td>';
+                echo '<td>' . $row['managerEmail'] . '</td>';
+                echo '<td>' . $row['garageURL'] . '</td>';
+                echo '<td>' . $row['overNight'] . '</td>';
+                echo '</tr>';
+                ?>
+        </table>
+    </body>
 </html>
--- viewall.php
+++ viewall.php
@@ -20,16 +20,20 @@ $statement = $gateway->getGarages();
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
-        <link rel="stylesheet" href="css/global.css">
-        <meta charset="UTF-8">
+
         <title>Base View of Garages</title>
-        <script src="javascript/delete.js"></script>
+        <!--all styles and scripts will be contained in these php scripts-->
+        <?php require 'utilities/styles.php'; ?>
+        <?php require 'utilities/scripts.php'; ?>
 
     </head>
     <body>
-        <div class="container_12">
-            <table class="pure-table pure-table-bordered">
+        <div class="row">
+            <?php require 'utilities/header.php'; ?>
+            <?php require 'utilities/toolbar.php'; ?>
+        </div>
+        <div class="container">
+            <table class="table-responsive">
                 <thead>
                     <tr>
                         <th>Address</th>
@@ -44,24 +48,25 @@ $statement = $gateway->getGarages();
                     $row = $statement->fetch(PDO::FETCH_ASSOC);
                     while ($row) {
 
-                        echo '<tr class="pure-table-odd">';
+                        echo '<tr class="info">';
                         echo '<td>' . $row['garageAddress'] . '</td>';
                         echo '<td>' . $row['phoneNo'] . '</td>';
                         echo '<td>' . $row['managerName'] . '</td>';
                         echo '<td>' . $row['nameofGarage'] . '</td>';
                         echo '<td>' . $row['garageID'] . '</td>';
-                        echo '<td>' . '<a href="viewGarage.php?id=' . $row['garageID'] . '"><img src="icons/search67.png" width="30px" height="30px" style="padding-right: 10px"/></a>'
-                        . '<a href="editgarageform.php"><img src="icons/edi.png" width="30px" height="30px"/></a>'
-                        . '<a class="deletebtn" href="deletegarage.php?id=' . $row['garageID'] . '"><img class="deletebtn" src="icons/del.png" style="padding-left: 10px" width="30px" height="30px"/></a>';
+                        echo '<td>'
+                        . '<a href="viewGarage.php?id=' . $row['garageID'] . '"><img src="icons/search67.png" height="40px" width="40px" style="padding-right: 10px"/></a>'
+                        . '<a href="editgarageform.php?id=' . $row['garageID'] . '"><img src="icons/edi.png" height="40px" width="40px"  /></a>'
+                        . '<a class="deletebtn" href="deletegarage.php?id=' . $row['garageID'] . '"><img class="deletebtn" src="icons/del.png"  height="40px" width="40px" style="padding-left: 10px"/></a>';
                         echo '</tr>';
 
                         $row = $statement->fetch(PDO::FETCH_ASSOC);
                     }
                     ?>
 
-
             </table>
-            <a href="addgarage.php"> <img src="icons/add.png" style="padding: 20px" width="40" height="40"></a>
+
+            <a href="addgarageform.php"> <img src="icons/add.png" width="40" height="40"></a>
         </div>
     </body>
 
