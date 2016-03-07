090a3c8739bdadb5c9200cd8658c25d0f2664770
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-07 17:18:31 +0000

    For connection to the DB

--- garageTableGateway.php
+++ garageTableGateway.php
@@ -6,17 +6,15 @@
  * Date: 26/11/2015
  * Time: 3:32 PM
  */
-class garageTableGateway
-{
+class garageTableGateway {
+
     private $connection;
 
-    public function __construct($conn)
-    {
+    public function __construct($conn) {
         $this->connection = $conn;
     }
 
-    public function getGarages()
-    {
+    public function getGarages() {
         $sqlQuery = "SELECT * FROM Web_Garage";
 
         $statement = $this->connection->prepare($sqlQuery);
@@ -27,12 +25,9 @@ class garageTableGateway
         }
 
         return $statement;
-
-
     }
 
-    public function getGarageByID($garageID)
-    {
+    public function getGarageByID($garageID) {
         $sqlQuery = "SELECT * FROM Web_Garage WHERE garageID = :garageID";
 
         $statement = $this->connection->prepare($sqlQuery);
@@ -45,17 +40,14 @@ class garageTableGateway
         }
 
         return $statement;
-
     }
 
-    public function insertGarage($garage)
-    {
-        $sqlQuery = "INSERT INTO Web_Garage(garageAddress, phoneNo, managerName, nameofGarage, garageID, dateService, managerEmail, garageURL, overNight)"  .
-            "VALUES (:garageAddress, :phoneNo, :managerName, :nameofGarage, :garageID, :dateService, :managerEmail, :garageURL, :overNight)";
+    public function insertGarage($garage) {
+        $sqlQuery = "INSERT INTO Web_Garage(garageAddress, phoneNo, managerName, nameofGarage, garageID, dateService, managerEmail, garageURL, overNight)" .
+                "VALUES (:garageAddress, :phoneNo, :managerName, :nameofGarage, :garageID, :dateService, :managerEmail, :garageURL, :overNight)";
 
         $statement = $this->connection->prepare($sqlQuery);
         $parameters = array(
-
             "garageAddress" => $garage->getAddress(),
             "phoneNo" => $garage->getPhone(),
             "managerName" => $garage->getManagerName(),
@@ -65,7 +57,6 @@ class garageTableGateway
             "managerEmail" => $garage->getManagerEmail(),
             "garageURL" => $garage->getGarageURL(),
             "overNight" => $garage->getOvernight()
-
         );
 
         $exec = $statement->execute($parameters);
@@ -77,11 +68,9 @@ class garageTableGateway
         $id = $this->connection->lastInsertId();
 
         return $id;
-
     }
 
-    public function removeGarage($id)
-    {
+    public function removeGarage($id) {
         $sqlQuery = "DELETE FROM Web_Garage WHERE garageID= :garageID";
 
         $statement = $this->connection->prepare($sqlQuery);
@@ -94,43 +83,48 @@ class garageTableGateway
         if (!$status) {
             die("Could not delete the garage");
         }
-
     }
 
-
-    public function updateGarage($add, $phone, $manager_name, $garage_name, $service_date, $manager_email, $garage_url, $overnight)
-    {
+    public function updateGarage($g) {
 
         $sqlQuery = "UPDATE Web_Garage SET " .
-            "garageAddress = :address, " .
-            "phoneNo = :phone, " .
-            "managerName = :manager_name, " .
-            "nameofGarage = :garage_name, " .
-            "dateService = :service_date, " .
-            "managerEmail = :manager_email," .
-            "garageURL = :garage_url, " .
-            "overNight = :overnight";
+                "garageAddress = :address, " .
+                "phoneNo = :phone, " .
+                "managerName = :manager_name, " .
+                "nameofGarage = :garage_name, " .
+                "dateService = :service_date, " .
+                "managerEmail = :manager_email," .
+                "garageURL = :garage_url, " .
+                "overNight = :overnight" .
+                " WHERE garageID = :id";
 
         $statement = $this->connection->prepare($sqlQuery);
         $parameters = array(
-            "address" => $add,
-            "phone" => $phone,
-            "manager_name" => $manager_name,
-            "garage_name" => $garage_name,
-            "service_date" => $service_date,
-            "manager_email" => $manager_email,
-            "garage_url" => $garage_url,
-            "overnight" => $overnight
+            "address" => $g->getAddress(),
+            "phone" => $g->getPhone(),
+            "manager_name" => $g->getManagerName(),
+            "garage_name" => $g->getGarageName(),
+            "service_date" => $g->getServiceDate(),
+            "manager_email" => $g->getManagerEmail(),
+            "garage_url" => $g->getGarageURL(),
+            "overnight" => $g->getOvernight(),
+            "id" => $g->getGarageID()
         );
+
+//        echo "<pre>";
+//        print_r($parameters);
+//        print_r($g);
+//        echo "</pre>";
 
         $status = $statement->execute($parameters);
 
-        if (!$status){
-            die("Could not insert garage");
+        if (!$status) {
+            die("Could not update garage");
         }
 
         $id = $this->connection->lastInsertId();
 
         return $id;
     }
+
 }
