388b00a76c1f29b8de851b0a5366d8c46f884e57
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-14 10:24:02 +0000

    Styling of the page chagned

--- addgarageform.php
+++ addgarageform.php
@@ -31,7 +31,7 @@ if (!isset($formdata)) {
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Add Bus Form</title>
+        <title>Add Garage Form</title>
         <!--all styles and scripts will be contained in these php scripts-->
         <?php require 'utilities/styles.php'; ?>
         <?php require 'utilities/scripts.php'; ?>
@@ -40,131 +40,133 @@ if (!isset($formdata)) {
     </head>
 
     <body>
-        <div class="row">
-            <?php require 'utilities/header.php' ?>
-            <?php require 'utilities/toolbar.php' ?>
-        </div>
+        <div class="container-fluid">
+                <?php require 'utilities/header.php'; ?>
         <!--All content in container-->
-        <div class="container">
             <!--All data will be displayed from database-->
-            <br>
-            <hr>
-            <h4>Garage Add</h4>
-            <hr>
+
             <!--    Opening databse area -->
-                <!--        Opening form area           -->
-                <form action="createGarage.php" id="databaseAdd" name="databaseAdd"
-                      method="POST" class="form-horizontal">
-                    <!--            Data will go inside of here -->
-                   
-                    <div class="">
-                        <div class="form-group">
-                            Garage Address
-                            <input type="text" id="garageAdd" name="garageAdd" class="form-control"
-                                   value="<?php setValue($formdata, 'garageAdd') ?>"/><span class="errors"
-                                   id="addresssError">
-                                <!--                        Garage address errors will go here          -->
-                                <?php
-                                if (isset($errors['garageAdd']))
-                                    echo $errors['garageAdd'];
-                                ?>
-                            </span>
+            <div class="row page-header home_content ">
+                            <br>
+            <hr>
+            <h4 class="center-content">Garage Add</h4>
+            <hr>
+            <!--        Opening form area           -->
+            <form action="createGarage.php" id="databaseAdd" name="databaseAdd"
+                  method="POST" class="form-horizontal col-lg-push-2 col-lg-8 col-lg-pull-2" >
+                <!--            Data will go inside of here -->
+                    <div class="form-group">
+                        Garage Address
+                        <input type="text" id="garageAdd" name="garageAdd" class="form-control"
+                               value="<?php setValue($formdata, 'garageAdd') ?>"/><span class="errors"
+                               id="addresssError">
+                            <!--                        Garage address errors will go here          -->
+                            <?php
+                            if (isset($errors['garageAdd']))
+                                echo $errors['garageAdd'];
+                            ?>
+                        </span>
 
-                        </div>
-                        <div class="form-group">
-                            Garage Phone
-                            <input type="text" id="phoneNo" name="phoneNo" class="form-control"
-                                   value="<?php setValue($formdata, 'phoneNo') ?>"/><span class="errors" id="phoneError">
-                                <!--                        Phone errors will go here       -->
-                                <?php
-                                if (isset($errors['phoneNo']))
-                                    echo $errors['phoneNo'];
-                                ?>
-                            </span>
+                    </div>
+                    <div class="form-group">
+                        Garage Phone
+                        <input type="text" id="phoneNo" name="phoneNo" class="form-control"
+                               value="<?php setValue($formdata, 'phoneNo') ?>"/><span class="errors" id="phoneError">
+                            <!--                        Phone errors will go here       -->
+                            <?php
+                            if (isset($errors['phoneNo']))
+                                echo $errors['phoneNo'];
+                            ?>
+                        </span>
 
-                        </div>
-                        <div class="form-group">
-                            Manager Name
-                            <input type="text" id="managerName" name="managerName" class="form-control"
-                                   value="<?php setValue($formdata, 'managerName') ?>"/><span class="errors"
-                                   id="mnameError">
-                                <!--                        Manager name errors will go here           -->
-                                <?php
-                                if (isset($errors['managerName']))
-                                    echo $errors['managerName'];
-                                ?>
-                            </span>
+                    </div>
+                    <div class="form-group">
+                        Manager Name
+                        <input type="text" id="managerName" name="managerName" class="form-control"
+                               value="<?php setValue($formdata, 'managerName') ?>"/><span class="errors"
+                               id="mnameError">
+                            <!--                        Manager name errors will go here           -->
+                            <?php
+                            if (isset($errors['managerName']))
+                                echo $errors['managerName'];
+                            ?>
+                        </span>
 
-                        </div>
+                    </div>
 
-                        <div class="form-group"> 
-                            Garage Name
+                    <div class="form-group"> 
+                        Garage Name
 
-                            <input type="text" id="garageName" name="garageName" class="form-control"
-                                   value="<?php setValue($formdata, 'garageName') ?>"/> <span class="errors"
-                                   id="garagenameError">
-                                       <?php
-                                       if (isset($errors['garageName']))
-                                           echo $errors['garageName'];
-                                       ?>
-                                <!--                        Garage Name Errors will go here             -->
-                            </span>
+                        <input type="text" id="garageName" name="garageName" class="form-control"
+                               value="<?php setValue($formdata, 'garageName') ?>"/> <span class="errors"
+                               id="garagenameError">
+                                   <?php
+                                   if (isset($errors['garageName']))
+                                       echo $errors['garageName'];
+                                   ?>
+                            <!--                        Garage Name Errors will go here             -->
+                        </span>
 
-                        </div>
-                        <div class="form-group">
-                            Date of Next Service
+                    </div>
+                    <div class="form-group">
+                        Date of Next Service
 
-                            <input type="text" id="serviceDate" name="serviceDate" class="form-control"
-                                   value="<?php setValue($formdata, 'serviceDate') ?>"/><span class="errors" id="dateError">
-                                <!--            Date Error will go here             -->
-                                <?php
-                                if (isset($errors['serviceDate']))
-                                    echo $errors['serviceDate'];
-                                ?>
+                        <input type="text" id="serviceDate" name="serviceDate" class="form-control"
+                               value="<?php setValue($formdata, 'serviceDate') ?>"/><span class="errors" id="dateError">
+                            <!--            Date Error will go here             -->
+                            <?php
+                            if (isset($errors['serviceDate']))
+                                echo $errors['serviceDate'];
+                            ?>
 
-                            </span>
+                        </span>
 
-                        </div>
-                        <div class="form-group">
-                            Manager Email
-                            <input type="text" id="manEmail" name="managerEmail" class="form-control" 
-                                   value="<?php setValue($formdata, 'managerEmail') ?>"/><span class="errors"
-                                   id="emailError">
-                                <!--                        Email Error will go here        -->
-                                <?php
-                                if (isset($errors['managerEmail']))
-                                    echo $errors['managerEmail'];
-                                ?>
-                            </span>
-                        </div>
-                        <div class="form-group">
-                            Garage URL
-                            <input type="text" id="gURL" name="garageURL" class="form-control" value="<?php setValue($formdata, 'garageURL') ?>"/><span
-                                class="errors" id="urlError">
-                                <!--          URL Error will go here         -->
-                                <?php
-                                if (isset($errors['garageURL']))
-                                    echo $errors['garageURL'];
-                                ?>
-                            </span>
+                    </div>
+                    <div class="form-group">
+                        Manager Email
+                        <input type="text" id="manEmail" name="managerEmail" class="form-control" 
+                               value="<?php setValue($formdata, 'managerEmail') ?>"/><span class="errors"
+                               id="emailError">
+                            <!--                        Email Error will go here        -->
+                            <?php
+                            if (isset($errors['managerEmail']))
+                                echo $errors['managerEmail'];
+                            ?>
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        Garage URL
+                        <input type="text" id="gURL" name="garageURL" class="form-control" value="<?php setValue($formdata, 'garageURL') ?>"/><span
+                            class="errors" id="urlError">
+                            <!--          URL Error will go here         -->
+                            <?php
+                            if (isset($errors['garageURL']))
+                                echo $errors['garageURL'];
+                            ?>
+                        </span>
 
-                        </div>
-                        <div class="form-group form-control">
-                            Overnight Facility?
-                            Yes<input type="checkbox" id="overnighty" type="checkbox" name="overnighty" value="Yes" <?php setChecked($formdata, 'overnighty', 'Yes') ?>/>
-                            No<input type="checkbox" id="overnightn" type="checkbox" name="overnightn" value="No"<?php setChecked($formdata, 'overnightn', 'No') ?>/>
-                        </div>
+                    </div>
+                    <div class="form-group form-control">
+                        Overnight Facility?
+                        Yes<input type="checkbox" id="overnighty" type="checkbox" name="overnighty" value="Yes" <?php setChecked($formdata, 'overnighty', 'Yes') ?>/>
+                        No<input type="checkbox" id="overnightn" type="checkbox" name="overnightn" value="No"<?php setChecked($formdata, 'overnightn', 'No') ?>/>
+                    </div>
 
-                        <div class="form-group">
-                            <input type="submit" id="addgarage" value="Add Garage"
-                                       name="add">
-                        </div>
-                        <!--        Closing data area       -->
-                        </div>
-                        <!--            Closing form area           -->
-                </form>
+                    <div class="form-group">
+                        <input type="submit" 
+                               id="addgarage" 
+                               value="Add Garage"
+                               name="add"
+                               class="btn btn-success">
+                    </div>
+                    <!--        Closing data area       -->
+
+                <!--            Closing form area           -->
+            </form>
+            </div>
             <!--Closing container-->
         </div>
+        </div><!--Closing container-->
         <?php require 'utilities/footer.php'; ?> 
     </body>
 </html>
