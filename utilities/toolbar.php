8921a0b53bbfec46f7af8d1d7e781e69872ff250
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-15 14:54:35 +0000

    toolbar has the whole nav set up for each apge and logged in

--- utilities/toolbar.php
+++ utilities/toolbar.php
@@ -1,18 +1,21 @@
-<ul>
-    <?php
-    require_once 'utilities/functions.php';
-    
-    echo '<li><a href"home.php">Tour Bus Massacre</a></li>';
-    if (!is_logged_in()){
-        echo '<li><a href="login_form.php">Login</a></li>';
-    }
-    else {
-        echo '<li><a href="landing.php">Home</a></li>';
-    }
-    
-    
-    ?>
-    
-    
-</ul>
+<?php
 
+require_once 'utilities/functions.php';
+start_session();
+
+$user = $_SESSION['user'];
+if (!is_logged_in()) {
+    echo '<ul class="nav navbar-nav navbar-right">';
+    echo '<li><a href="login_form.php">Login</a></li>';
+    echo '<li><a href="register_form.php">Register</a></li>';
+    echo '<li><a href="landing.php">Home</a></li>';
+    echo '</ul>';
+} else {
+    echo '<ul class="nav navbar-nav navbar-right">';
+    echo '<li><a href="landing.php">Home</a></li>';
+    echo '<li><a href="viewall.php">Garages</a></li>';
+    echo '<li><a href="viewallbus.php">Buses</a></li>';
+    echo '<li><a href="logout.php">Logout</a></li>';
+    echo '</ul>';
+}
+?>
