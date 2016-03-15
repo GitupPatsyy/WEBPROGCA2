4b70c7df34bf092591b94bf89a1ebd307bcb3dd2
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-15 14:53:47 +0000

    header for most pages

--- utilities/header.php
+++ utilities/header.php
@@ -1,8 +1,44 @@
 <?php require_once 'utilities/styles.php'; ?>
 <div class="row header_style">
     <div class="jumbotron">
-        <h2 class="left-content">Tour Bus Massacre</h2>
-        <?php require 'utilities/toolbar.php'; ?>
+        <nav class="nav-bar navbar-default">
+            <div class="navbar-header">Tour Bus Massacre</div>
+            <?php require 'utilities/toolbar.php'; ?>
+        </nav>
+        <div class="row col-lg-12">
+            <div class="row col-lg-3 nav-subtext">
+                <p><strong>Killing</strong> tours since <i>1993&reg</i></p>
+                <div class="jumbo-content"></div>
+            </div>
+            <hr class="nav-hr"/>
+
+            <div class="row col-lg-12">
+                <div class="feature col-lg-4 icon-box">
+                    <img src="img/icons/bus (2).png" class="center-image" />
+                    <h2 class="center-content">Bus</h2>
+                    <hr />
+                    <p>
+                        Users will be able to CRUD on some buses and explore our mystical system to see what we have in store.
+                    </p>
+                </div>
+                <div class="feature col-lg-4 icon-box">
+                    <img src="img/icons/oil (1).png" class="center-image" />
+                    <h2 class="center-content">Garage</h2>
+                    <hr />
+                    <p>
+                        Users will be able to CRUD on some garages and explore our mystical system to see what we have in store.
+                    </p>
+                </div>
+                <div class="feature col-lg-4 icon-box">
+                    <img src="img/icons/seat-recline-normal (1).png" class="center-image" />
+                    <h2 class="center-content">Driver</h2>
+                    <hr />
+                    <p>
+                        Users will be able to CRUD on some drivers and explore our mystical system to see what we have in store.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div><!-- Closing jumbotron-->
 </div>
 
