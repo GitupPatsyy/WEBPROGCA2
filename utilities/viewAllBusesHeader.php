b7469cb9b5a5f720790bb7ec9dd2c292c62ba082
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-15 14:53:25 +0000

    Jumbotron closing pos changed.

--- utilities/viewAllBusesHeader.php
+++ utilities/viewAllBusesHeader.php
@@ -7,47 +7,52 @@
             <?php require 'utilities/toolbar.php'; ?>
         </nav>
         <div class="row col-lg-12">
+            <div class="row col-lg-3 nav-subtext">
+                <p><strong>Killing</strong> tours since <i>1993&reg</i></p>
+                <div class="jumbo-content"></div>
+            </div>
             <hr class="nav-hr"/>
-<table class="table table-bordered table-hover">
-
-                <div class="row bus-table col-lg-push-2 col-lg-8 col-lg-pull-2">
-                    <h4 class="center-content">View All Buses</h4>
-                    <a href="addbusform.php"> <img src="icons/add.png" width="40" height="40"  style="margin: 3px;" ></a>
-                </div>
-                <thead>
-                    <tr>
-                        <th>Reg Number</th>
-                        <th>Make</th>
-                        <th>Model</th>
-                        <th>Engine Size</th>
-                        <th>Date Bought</th>
-                        <th>Actions</th>
-
-                    </tr>
-                    <?php
-                    $row = $statement->fetch(PDO::FETCH_ASSOC);
-                    while ($row) {
-
-                        echo '<tr class="info">';
-                        echo '<td>' . $row['regNum'] . '</td>';
-                        echo '<td>' . $row['busMake'] . '</td>';
-                        echo '<td>' . $row['busModel'] . '</td>';
-                        echo '<td>' . $row['engineSize'] . '</td>';
-                        echo '<td>' . $row['dateBought'] . '</td>';
-                        echo '<td>'
-                        . '<a href="viewBus.php?id=' . $row['busID'] . '"><img src="icons/view.png"  height="40px" width="40px"  style="margin: 3px;"  /></a>'
-                        . '<a href="editbusform.php?id=' . $row['busID'] . '"><img src="icons/edit.png" height="40px" width="40px"   style="margin: 3px;" /></a>'
-                        . '<a class="deletebtn" href="deletebus.php?id=' . $row['busID'] . '"><img class="" src="icons/delete.png"  height="40px" width="40px" style="margin: 3px;" /></a>';
-                        echo '</tr>';
-
-                        $row = $statement->fetch(PDO::FETCH_ASSOC);
-                    }
-                    ?>
-            
-            </table>
-
-
         </div>
-</div><!-- Closing jumbotron-->
+    </div><!-- Closing jumbotron-->
 </div>
+
+<div class="row col-lg-10 col-lg-offset-1">
+    <table class="table rpb-table">
+
+            <h4 class="center-content">View All Buses</h4>
+            <a href="addbusform.php"> <img src="icons/add.png" width="40" height="40"  style="margin: 3px;" ></a>
+        
+        <thead>
+            <tr>
+                <th>Reg Number</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Engine Size</th>
+                <th>Date Bought</th>
+                <th>Actions</th>
+
+            </tr>
+            <?php
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+            while ($row) {
+
+                echo '<tr class="info">';
+                echo '<td>' . $row['regNum'] . '</td>';
+                echo '<td>' . $row['busMake'] . '</td>';
+                echo '<td>' . $row['busModel'] . '</td>';
+                echo '<td>' . $row['engineSize'] . '</td>';
+                echo '<td>' . $row['dateBought'] . '</td>';
+                echo '<td>'
+                . '<a href="viewBus.php?id=' . $row['busID'] . '"><img src="icons/view.png"  height="30px" width="30px"  style="margin: 3px;"  /></a>'
+                . '<a href="editbusform.php?id=' . $row['busID'] . '"><img src="icons/edit.png" height="30px" width="30px"   style="margin: 3px;" /></a>'
+                . '<a class="deletebtn" href="deletebus.php?id=' . $row['busID'] . '"><img class="" src="icons/delete.png"  height="30px" width="30px" style="margin: 3px;" /></a>';
+                echo '</tr>';
+
+                $row = $statement->fetch(PDO::FETCH_ASSOC);
+            }
+            ?>
+
+    </table>
+</div>
+
 
--- utilities/viewAllGaragesHead.php
+++ utilities/viewAllGaragesHead.php
@@ -7,45 +7,47 @@
             <?php require 'utilities/toolbar.php'; ?>
         </nav>
         <div class="row col-lg-12">
+            <div class="row col-lg-3 nav-subtext">
+                <p><strong>Killing</strong> tours since <i>1993&reg</i></p>
+                <div class="jumbo-content"></div>
+            </div>
             <hr class="nav-hr"/>
-<table class="table table-bordered table-hover">
-
-                <div class="row garage-table col-lg-push-2 col-lg-8 col-lg-pull-2">
-                    <h4 class="center-content">View All Garages</h4>
-                    <a href="addgarageform.php"> <img src="icons/add.png" width="40" height="40"  style="margin: 3px;" ></a>
-                </div>
-                <thead>
-                    <tr>
-                        <th>Address</th>
-                        <th>Phone No.</th>
-                        <th>Manager Name</th>
-                        <th>Garage Name</th>
-                        <th>Garage ID</th>
-                        <th>Actions</th>
-
-                    </tr>
-                    <?php
-                    $row = $statement->fetch(PDO::FETCH_ASSOC);
-                    while ($row) {
-
-                        echo '<tr class="info">';
-                        echo '<td>' . $row['garageAddress'] . '</td>';
-                        echo '<td>' . $row['phoneNo'] . '</td>';
-                        echo '<td>' . $row['managerName'] . '</td>';
-                        echo '<td>' . $row['nameofGarage'] . '</td>';
-                        echo '<td>' . $row['garageID'] . '</td>';
-                        echo '<td>'
-                        . '<a href="viewGarage.php?id=' . $row['garageID'] . '"><img src="icons/view.png"  height="40px" width="40px"  style="margin: 3px;"  /></a>'
-                        . '<a href="editgarageform.php?id=' . $row['garageID'] . '"><img src="icons/edit.png" height="40px" width="40px"   style="margin: 3px;" /></a>'
-                        . '<a class="deletebtn" href="deletegarage.php?id=' . $row['garageID'] . '"><img class="" src="icons/delete.png"  height="40px" width="40px" style="margin: 3px;" /></a>';
-                        echo '</tr>';
-
-                        $row = $statement->fetch(PDO::FETCH_ASSOC);
-                    }
-                    ?>
-
-            </table>
-    </div>
-</div><!-- Closing jumbotron-->
+        </div>
+    </div><!-- Closing jumbotron-->
 </div>
+            <div class="row col-lg-10 col-lg-offset-1"">
+                <table class="table rpb-table">
+                        <h4 class="center-content">View All Garages</h4>
+                        <a href="addgarageform.php"> <img src="icons/add.png" width="40" height="40"  style="margin: 3px;" ></a>
+                    <thead>
+                        <tr>
+                            <th>Address</th>
+                            <th>Manager Name</th>
+                            <th>Garage Name</th>
+                            <th>Garage ID</th>
+                            <th>Actions</th>
+
+                        </tr>
+                        <?php
+                        $row = $statement->fetch(PDO::FETCH_ASSOC);
+                        while ($row) {
+
+                            echo '<tr class="info">';
+                            echo '<td>' . $row['garageAddress'] . '</td>';
+                            echo '<td>' . $row['managerName'] . '</td>';
+                            echo '<td>' . $row['nameofGarage'] . '</td>';
+                            echo '<td>' . $row['garageID'] . '</td>';
+                            echo '<td>'
+                            . '<a href="viewGarage.php?id=' . $row['garageID'] . '"><img src="icons/view.png"  height="30px" width="30px"  style="margin: 3px;"  /></a>'
+                            . '<a href="editgarageform.php?id=' . $row['garageID'] . '"><img src="icons/edit.png" height="30px" width="30px"   style="margin: 3px;" /></a>'
+                            . '<a class="deletebtn" href="deletegarage.php?id=' . $row['garageID'] . '"><img class="" src="icons/delete.png"  height="30px" width="30px" style="margin: 3px;" /></a>';
+                            echo '</tr>';
+
+                            $row = $statement->fetch(PDO::FETCH_ASSOC);
+                        }
+                        ?>
+
+                </table>
+            </div>
+
 
