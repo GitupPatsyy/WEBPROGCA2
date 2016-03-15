ae967265b228b5f1774dfa7792506f1a4da2b5ba
GitupPatsyy <roryplunkettboyle12@gmail.com>
2016-03-15 14:54:04 +0000

    badly needs to be styled

--- utilities/footer.php
+++ utilities/footer.php
@@ -1,2 +1,2 @@
 <?php require_once 'utilities/styles.php'; ?>
-<h3 class="center-content foot">&COPY; Tour Bus Massacre 2016</h3>
+<footer class="left-content foot">&COPY; Tour Bus Massacre 2016</footer>
